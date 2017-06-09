f10dcc7432cc96cb37945154800770c0e50e2c0a
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-09 18:48:47 -0400

    Add an initial `bld.bat`

---
+++ recipe/bld.bat
@@ -0,0 +1,25 @@
+set CMAKE_CONFIG=Release
+if errorlevel 1 exit 1
+
+mkdir build_%CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
+cd build_%CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
+cmake .. ^
+        -G "NMake Makefiles" ^
+        -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+        -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+        "%SRC_DIR%"
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake check_python
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
