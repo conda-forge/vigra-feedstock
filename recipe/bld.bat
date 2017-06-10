18df6d30cf74679ccc27ea051aefc38c1deb9157
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-09 20:31:12 -0400

    Enable HDF5 dynamic libraries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,6 +12,7 @@ cmake .. ^
         -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+        -DCMAKE_CXX_FLAGS="%CXXFLAGS% -DH5_BUILT_AS_DYNAMIC_LIB" ^
         "%SRC_DIR%"
 if errorlevel 1 exit 1
 
